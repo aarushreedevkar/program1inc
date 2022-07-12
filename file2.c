ec28154d873dfc0c09abb9c8cede4fa05f70698c
jyoti <jyotidhage10@gmail.com>
2022-07-12 09:34:14 +0530

    {jyoti}cretaed 2nd program in c

---
+++ file2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int arr[5],i,sum=0
+printf("enter five numbers");
+for(i=0;i<5:i++);
+{
+scanf("%d".&arr[i])
+}
+for(i=0;i<5;i++)
+{
+printf("/n the sum of given 5 numbers is :%d\n",sum)
+}
